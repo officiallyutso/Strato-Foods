f27b7f0e28c80cfbb941efec747c0cd7d11e3a82
UTSO SARKAR <62977856+officiallyutso@users.noreply.github.com>
2024-07-06 17:37:23 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\Programs\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\projects\\Apps\\Flutter Porjects\\Foody\\foody" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\Programs\\flutter"
+  "PROJECT_DIR=D:\\projects\\Apps\\Flutter Porjects\\Foody\\foody"
+  "FLUTTER_ROOT=D:\\Programs\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\projects\\Apps\\Flutter Porjects\\Foody\\foody\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\projects\\Apps\\Flutter Porjects\\Foody\\foody"
+  "FLUTTER_TARGET=D:\\projects\\Apps\\Flutter Porjects\\Foody\\foody\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9iYjEwYzU0NjY2MzhlOTYzNDc5YmE1ZTY0ZTYwMWU0MmQxYTQzNDQ3Lw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\projects\\Apps\\Flutter Porjects\\Foody\\foody\\.dart_tool\\package_config.json"
+)
